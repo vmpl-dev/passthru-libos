03e1076edcea2bb78ceb0b345c48529953daccf5
Chia-Che Tsai <chiache@tamu.edu>
2020-01-10 07:11:08 -0600

    Add shim init and loader code

--- libos-c/shim_defs.h
+++ libos-c/shim_defs.h
@@ -3,6 +3,15 @@
 
 #define SYSCALLNR  340
 
+#ifdef __x86_64__
+# include "sysdep-x86_64.h"
+#endif
+
+#define IS_ERR INTERNAL_SYSCALL_ERROR
+#define IS_ERR_P INTERNAL_SYSCALL_ERROR_P
+#define ERRNO INTERNAL_SYSCALL_ERRNO
+#define ERRNO_P INTERNAL_SYSCALL_ERRNO_P
+
 #define __UNUSED(x) do { (void)(x); } while (0)
 
 #endif
